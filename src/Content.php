5daf23c8052e1c6cf588f23b2ea8afe4d8402e74
Tobias Bulla <tobias.bulla@condenast.de>
2017-07-18 17:01:11 +0200

    thumbnail override and Content->getImage() methods added

---
+++ src/Content.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bolt\Extension\CND\ImageService;
+
+class Content extends \Bolt\Legacy\Content {
+
+    /**
+     * Get the first image in the content.
+     *
+     * @return string
+     */
+    public function getImage() {
+
+        $result = parent::getImage();
+        if($result)
+            return $result;
+
+        // Grab the first field of type 'imageservicelist', and return that.
+        foreach ($this->contenttype['fields'] as $key => $field) {
+            if ($field['type'] === 'imageservicelist' && isset($this->values[$key])) {
+                $value = $this->values[$key];
+                return isset($value["items"][0]) ? $value["items"][0] : '';
+            }
+        }
+
+        // otherwise, no image.
+        return '';
+    }
+
+
+}
